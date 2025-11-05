e803ad48dadb19365a6f0861e72831ec4e508dd6
ethantseng <tseng8851@mydusd.org>
2025-11-05 11:21:01 -0800

    Add files via upload

---
+++ GoalKeeper.java
@@ -0,0 +1,13 @@
+/*
+ * Activity 3.1.1
+ */
+public class GoalKeeper
+{
+  public static void main(String[] args)
+  {    
+    int[] goals = {1, 2, 0, 3, 2, 4, 2, 1, 0, 2, 0, 1, 3, 2};
+
+    System.out.println(goals);
+
+  }
+}
