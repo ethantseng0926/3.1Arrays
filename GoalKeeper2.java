92554ee583df6417b5d8df8e06b036edd76d7fba
ethantseng <tseng8851@mydusd.org>
2025-11-05 11:28:20 -0800

    Update GoalKeeper2.java

--- GoalKeeper2.java
+++ GoalKeeper2.java
@@ -6,6 +6,7 @@ public class GoalKeeper2
   public static void main(String[] args)
   {    
     int[] ourGoals = new int[28];
-
+    System.out.println(ourGoals[5]); // goals scored in 6th game
   }
+
 }
